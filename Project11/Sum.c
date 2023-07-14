ead38e50a49245c927fd81daa01cd8c9672afc8a
sksjw95 <tjwldnd95@hanmail.net>
2023-07-15 00:40:53 +0900

    초기 커밋.

---
+++ Project11/Sum.c
@@ -0,0 +1,4 @@
+static int result;
+void Sum(data1, data2) {
+	result = data1 + data2;
+}
---
+++ Project11/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void Sum(int data1, int data2);
+ int result;
+void main() {
+	Sum(5, 3);
+	printf(" 5 + 3 = %d\n", result);
+}
